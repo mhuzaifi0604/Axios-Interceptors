212c6d26e4ee068fcb8d75fe55abd447033cc25e
mhuzaifi0604 <huzzaifaasim@gmail.com>
2023-07-08 00:04:50 +0500

    Initialized project

---
+++ LockUp/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        This is Page 01
+        </div>
+    </>
+  )
+}
+
+export default App
